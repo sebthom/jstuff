5f6dec12af4ac76a8e5d5386620c296c43aea92f
sebthom <sebthom@users.noreply.github.com>
2011-01-04 00:23:00 +0000

    update doc

--- jstuff-integration/src/main/java/net/sf/jstuff/integration/atom/feed/SimpleEntry.java
+++ jstuff-integration/src/main/java/net/sf/jstuff/integration/atom/feed/SimpleEntry.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Portions created by Sebastian Thomschke are copyright (c) 2005-2011 Sebastian
+ * Thomschke.
+ * 
+ * All Rights Reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sebastian Thomschke - initial implementation.
+ *******************************************************************************/
 package net.sf.jstuff.integration.atom.feed;
 
 import java.util.Date;
