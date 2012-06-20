5f083b5e31a4894a99846152754813c5b55a0f8c
sebthom <sebthom@users.noreply.github.com>
2012-06-20 19:36:52 +0000

    StringToMapConverter

---
+++ jstuff-integration/src/main/java/net/sf/jstuff/integration/spring/convert/AbstractConverter.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Portions created by Sebastian Thomschke are copyright (c) 2005-2012 Sebastian
+ * Thomschke.
+ * 
+ * All Rights Reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sebastian Thomschke - initial implementation.
+ *******************************************************************************/
+package net.sf.jstuff.integration.spring.convert;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.ConditionalGenericConverter;
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public abstract class AbstractConverter implements ConditionalGenericConverter
+{
+	@Autowired
+	protected ConversionService conversionService;
+
+	public AbstractConverter()
+	{
+		super();
+	}
+
+	public AbstractConverter(final ConversionService conversionService)
+	{
+		this.conversionService = conversionService;
+	}
+
+	public ConversionService getConversionService()
+	{
+		return conversionService;
+	}
+
+	public boolean matches(final TypeDescriptor sourceType, final TypeDescriptor targetType)
+	{
+		if (!conversionService.canConvert(sourceType, targetType.getMapKeyTypeDescriptor())) return false;
+
+		final boolean matches = conversionService.canConvert(sourceType, targetType.getMapValueTypeDescriptor());
+		return matches;
+	}
+
+	public void setConversionService(final ConversionService conversionService)
+	{
+		this.conversionService = conversionService;
+	}
+
+}
---
+++ jstuff-integration/src/main/java/net/sf/jstuff/integration/spring/convert/StringToMapConverter.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Portions created by Sebastian Thomschke are copyright (c) 2005-2012 Sebastian
+ * Thomschke.
+ * 
+ * All Rights Reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sebastian Thomschke - initial implementation.
+ *******************************************************************************/
+package net.sf.jstuff.integration.spring.convert;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class StringToMapConverter extends AbstractConverter
+{
+	private static final String ENTRY_SEPARATOR = ";";
+	private static final String VALUE_ASSIGNMENT = "=>";
+
+	public StringToMapConverter()
+	{
+		super();
+	}
+
+	public StringToMapConverter(final ConversionService conversionService)
+	{
+		super(conversionService);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Object convert(final Object source, final TypeDescriptor sourceType, final TypeDescriptor targetType)
+	{
+		if (source == null) return null;
+		final String sourceString = (String) source;
+		if (StringUtils.isBlank(sourceString)) return Collections.emptyMap();
+
+		@SuppressWarnings("rawtypes")
+		final Map result = new HashMap();
+
+		for (final String entry : sourceString.split(ENTRY_SEPARATOR))
+		{
+			final String[] entryArr = entry.split(VALUE_ASSIGNMENT);
+			final String key = entryArr[0].trim();
+			final String value = entryArr[1].trim();
+
+			final Object targetKey = conversionService
+					.convert(key, sourceType, targetType.getMapKeyTypeDescriptor(key));
+
+			result.put(targetKey, conversionService.convert(value, sourceType, targetType.getMapValueTypeDescriptor()));
+		}
+
+		return result;
+	}
+
+	public Set<ConvertiblePair> getConvertibleTypes()
+	{
+		return Collections.singleton(new ConvertiblePair(String.class, Map.class));
+	}
+
+}
