eae18df40726caa029dc8df5a4db5c892e6d4d9e
sebthom <sebthom@users.noreply.github.com>
2013-08-02 22:42:21 +0000

    isToday(Date)

--- jstuff-core/src/main/java/net/sf/jstuff/core/date/DateUtils.java
+++ jstuff-core/src/main/java/net/sf/jstuff/core/date/DateUtils.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Portions created by Sebastian Thomschke are copyright (c) 2005-2013 Sebastian
  * Thomschke.
- * 
+ *
  * All Rights Reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Sebastian Thomschke - initial implementation.
  *******************************************************************************/
@@ -88,8 +88,13 @@ public abstract class DateUtils extends org.apache.commons.lang3.time.DateUtils
 		return cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 	}
 
+	public static boolean isToday(final Date date)
+	{
+		return isSameDay(date, new Date());
+	}
+
 	/**
-	 * 
+	 *
 	 * @param year
 	 * @param month January = 1
 	 * @param day
