5003e3165ee545ef5d8da4d46ea380ec951dde50
Sebastian Thomschke <sebthom@users.noreply.github.com>
2017-10-17 18:43:23 +0200

    project cleanup

--- mvn-release-no-tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mvn -e clean release:prepare -Darguments="-DskipTests" && mvn -e release:perform -Darguments="-DskipTests"
--- mvn-release.sh
+++ mvn-release.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-mvn -e clean release:prepare && mvn -e release:perform
+#!/bin/bash
+REM to skip tests execute "mvn-release.sh -DskipTests"
+mvn -e clean release:prepare -Darguments="$@" && mvn -e release:perform -Darguments="$@"
