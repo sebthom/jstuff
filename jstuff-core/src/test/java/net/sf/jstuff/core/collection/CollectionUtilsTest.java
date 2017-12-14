dcdc777e04bd183b7e236f3681db7d185b2abf9a
Sebastian Thomschke <sebthom@users.noreply.github.com>
2017-12-14 15:09:30 +0100

    add intersect

--- jstuff-core/src/test/java/net/sf/jstuff/core/collection/CollectionUtilsTest.java
+++ jstuff-core/src/test/java/net/sf/jstuff/core/collection/CollectionUtilsTest.java
@@ -12,7 +12,11 @@
  *******************************************************************************/
 package net.sf.jstuff.core.collection;
 
+import static net.sf.jstuff.core.collection.CollectionUtils.*;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
@@ -21,23 +25,51 @@ import junit.framework.TestCase;
  */
 public class CollectionUtilsTest extends TestCase {
 
-    public void testHead() {
-        final List<String> testList = CollectionUtils.newArrayList("1", "2", "3");
+    @SuppressWarnings("unchecked")
+    public void testIntersectListt() {
+        assertEquals(0, intersect((List[]) null).size());
+        assertEquals(0, intersect(Collections.emptyList()).size());
+        assertEquals(0, intersect(Collections.emptyList(), null).size());
 
-        assertEquals(CollectionUtils.newArrayList("1"), CollectionUtils.head(testList, 1));
-        assertEquals(CollectionUtils.newArrayList("1", "2"), CollectionUtils.head(testList, 2));
-        assertEquals(CollectionUtils.newArrayList("1", "2", "3"), CollectionUtils.head(testList, 3));
-        assertEquals(CollectionUtils.newArrayList("1", "2", "3"), CollectionUtils.head(testList, 10));
-        assertEquals(CollectionUtils.newArrayList(), CollectionUtils.head(testList, -10));
+        final List<String> list1 = newArrayList("foo", "bar", "dog");
+        final List<String> list2 = newArrayList("cat", "bar", "foo");
+
+        assertEquals(2, intersect(list1, list2).size());
+        assertEquals("foo", intersect(list1, list2).iterator().next());
+        assertEquals(newArrayList("foo", "bar"), intersect(list1, list2));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testIntersectSet() {
+        assertEquals(0, intersect((Set[]) null).size());
+        assertEquals(0, intersect(Collections.emptySet()).size());
+        assertEquals(0, intersect(Collections.emptySet(), null).size());
+
+        final Set<String> set1 = newLinkedHashSet("foo", "bar", "dog");
+        final Set<String> set2 = newLinkedHashSet("cat", "bar", "foo");
+
+        assertEquals(2, intersect(set1, set2).size());
+        assertEquals("foo", intersect(set1, set2).iterator().next());
+        assertEquals(newLinkedHashSet("foo", "bar"), intersect(set1, set2));
+    }
+
+    public void testHead() {
+        final List<String> testList = newArrayList("1", "2", "3");
+
+        assertEquals(newArrayList("1"), head(testList, 1));
+        assertEquals(newArrayList("1", "2"), head(testList, 2));
+        assertEquals(newArrayList("1", "2", "3"), head(testList, 3));
+        assertEquals(newArrayList("1", "2", "3"), head(testList, 10));
+        assertEquals(newArrayList(), head(testList, -10));
     }
 
     public void testTail() {
-        final List<String> testList = CollectionUtils.newArrayList("1", "2", "3");
+        final List<String> testList = newArrayList("1", "2", "3");
 
-        assertEquals(CollectionUtils.newArrayList("3"), CollectionUtils.tail(testList, 1));
-        assertEquals(CollectionUtils.newArrayList("2", "3"), CollectionUtils.tail(testList, 2));
-        assertEquals(CollectionUtils.newArrayList("1", "2", "3"), CollectionUtils.tail(testList, 3));
-        assertEquals(CollectionUtils.newArrayList("1", "2", "3"), CollectionUtils.tail(testList, 10));
-        assertEquals(CollectionUtils.newArrayList(), CollectionUtils.tail(testList, -10));
+        assertEquals(newArrayList("3"), tail(testList, 1));
+        assertEquals(newArrayList("2", "3"), tail(testList, 2));
+        assertEquals(newArrayList("1", "2", "3"), tail(testList, 3));
+        assertEquals(newArrayList("1", "2", "3"), tail(testList, 10));
+        assertEquals(newArrayList(), tail(testList, -10));
     }
 }
