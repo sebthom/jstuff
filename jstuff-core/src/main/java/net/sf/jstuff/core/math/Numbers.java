25544b6ce0b1560642d15fe0b84cab0441190696
sebthom <sebthom@users.noreply.github.com>
2018-06-03 00:11:10 +0200

    add isInteger

--- jstuff-core/src/main/java/net/sf/jstuff/core/math/Numbers.java
+++ jstuff-core/src/main/java/net/sf/jstuff/core/math/Numbers.java
@@ -19,8 +19,6 @@ import java.util.UUID;
 import net.sf.jstuff.core.validation.Args;
 
 /**
- * Base62 = Duosexagesimal
- *
  * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
  */
 public class Numbers extends org.apache.commons.lang3.math.NumberUtils {
@@ -28,8 +26,16 @@ public class Numbers extends org.apache.commons.lang3.math.NumberUtils {
     public static final int MILLION = 1000 * 1000;
     public static final int BILLION = 1000 * MILLION;
 
+    private static final BigInteger INTEGER_MAX_VALUE = BigInteger.valueOf(Integer.MAX_VALUE);
+    private static final BigInteger INTEGER_MIN_VALUE = BigInteger.valueOf(Integer.MIN_VALUE);
+
     private static final BigInteger LONG_MAX_VALUE = BigInteger.valueOf(Long.MAX_VALUE);
     private static final BigInteger LONG_MIN_VALUE = BigInteger.valueOf(Long.MIN_VALUE);
+
+    public static boolean isInteger(final BigInteger number) {
+        Args.notNull("number", number);
+        return INTEGER_MAX_VALUE.compareTo(number) >= 0 && INTEGER_MIN_VALUE.compareTo(number) <= 0;
+    }
 
     public static boolean isLong(final BigInteger number) {
         Args.notNull("number", number);
@@ -39,9 +45,12 @@ public class Numbers extends org.apache.commons.lang3.math.NumberUtils {
     public static BigInteger toBigInteger(final UUID uuid) {
         if (uuid == null)
             return null;
-        return new BigInteger(1, ByteBuffer.wrap(new byte[16]) //
-            .putLong(uuid.getMostSignificantBits()) //
-            .putLong(uuid.getLeastSignificantBits()) //
-            .array());
+
+        return new BigInteger(1, //
+            ByteBuffer.wrap(new byte[16]) //
+                .putLong(uuid.getMostSignificantBits())//
+                .putLong(uuid.getLeastSignificantBits())//
+                .array() //
+        );
     }
 }
