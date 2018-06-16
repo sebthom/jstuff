b74cfa1e204bfd7e0abfcf76600a84ed3aa13a91
sebthom <sebthom@users.noreply.github.com>
2018-06-16 17:16:12 +0200

    Cleanup

--- mvn-release.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-REM to skip tests execute "mvn-release.cmd -DskipTests"
-call mvn %MVN_OPTS% -e release:prepare -Darguments="%MVN_OPTS% %*" && call mvn %MVN_OPTS% -e release:perform -Darguments="%MVN_OPTS% %*"
--- mvn-release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-REM to skip tests execute "mvn-release.sh -DskipTests"
-mvn -e clean release:prepare -Darguments="$@" && mvn -e release:perform -Darguments="$@"
