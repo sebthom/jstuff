ef68bc24f81a32c00ecde90603b051fd928935c3
Sebastian Thomschke <sebthom@users.noreply.github.com>
2018-07-11 13:22:09 +0200

    Add BlockingExecutor

---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/concurrent/BlockingExecutor.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Portions created by Sebastian Thomschke are copyright (c) 2010-2018 Sebastian
+ * Thomschke.
+ *
+ * All Rights Reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     Sebastian Thomschke - initial implementation.
+ *******************************************************************************/
+package net.sf.jstuff.core.concurrent;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+import net.sf.jstuff.core.Strings;
+import net.sf.jstuff.core.validation.Args;
+
+/**
+ * Executor that blocks submission of new tasks if a given maximum number of tasks are already pending.
+ *
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class BlockingExecutor implements Executor {
+
+   protected static final class RunnableWrapper implements Runnable {
+      private final Runnable wrapped;;
+      private final Semaphore limiter;
+
+      protected RunnableWrapper(final Runnable wrapped, final Semaphore limiter) {
+         this.wrapped = wrapped;
+         this.limiter = limiter;
+      }
+
+      public void run() {
+         try {
+            wrapped.run();
+         } finally {
+            limiter.release();
+         }
+      }
+   }
+
+   protected final Semaphore limiter;
+   protected final int maxWaitTime;
+   protected final TimeUnit maxWaitTimeUnit;
+   protected final Executor wrapped;
+
+   protected final int maxPendingTasks;
+
+   /**
+    * @param maxWaitTime max time to wait for a tasks being added to the queue
+    */
+   public BlockingExecutor(final Executor executor, final int maxPendingTasks, final int maxWaitTime, final TimeUnit maxWaitTimeUnit) {
+      Args.notNull("executor", executor);
+      Args.min("maxPendingTasks", maxPendingTasks, 1);
+      Args.notNegative("maxWaitTime", maxWaitTime);
+      Args.notNull("maxWaitTimeUnit", maxWaitTimeUnit);
+
+      wrapped = executor;
+      this.maxPendingTasks = maxPendingTasks;
+      this.maxWaitTime = maxWaitTime;
+      this.maxWaitTimeUnit = maxWaitTimeUnit;
+      limiter = new Semaphore(maxPendingTasks);
+   }
+
+   protected void aquirePermit() throws RejectedExecutionException {
+      try {
+         if (!limiter.tryAcquire(maxWaitTime, maxWaitTimeUnit))
+            throw new RejectedExecutionException("Executor '" + wrapped + "' is busy!");
+      } catch (final InterruptedException e) {
+         Thread.currentThread().interrupt();
+         throw new IllegalStateException(e);
+      }
+   }
+
+   public final void execute(final Runnable command) throws RejectedExecutionException {
+      Args.notNull("command", command);
+
+      aquirePermit();
+
+      try {
+         wrapped.execute(new RunnableWrapper(command, limiter));
+      } catch (final RuntimeException ex) {
+         limiter.release();
+         throw ex;
+      }
+   }
+
+   @Override
+   public final String toString() {
+      return Strings.toString(this, //
+         "availablePermits", limiter.availablePermits(), //
+         "maxPendingTasks", maxPendingTasks, //
+         "maxWaitTime", maxWaitTime, //
+         "maxWaitTimeUnit", maxWaitTimeUnit, //
+         "wrapped", wrapped //
+      );
+   }
+}
