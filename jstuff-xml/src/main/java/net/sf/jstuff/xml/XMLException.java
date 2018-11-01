8c3a646daff746716cefde1b1fba7e1bb32b5d5b
sebthom <sebthom@users.noreply.github.com>
2018-11-01 16:58:21 +0100

    Update file header

--- jstuff-xml/src/main/java/net/sf/jstuff/xml/XMLException.java
+++ jstuff-xml/src/main/java/net/sf/jstuff/xml/XMLException.java
@@ -1,20 +1,17 @@
-/*******************************************************************************
- * Portions created by Sebastian Thomschke are copyright (c) 2010-2018 Sebastian
- * Thomschke.
- * 
- * All Rights Reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- * 
- * Contributors:
- *     Sebastian Thomschke - initial implementation.
- *******************************************************************************/
+/*********************************************************************
+ * Copyright 2010-2018 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
 package net.sf.jstuff.xml;
 
 /**
  * Wrapping runtime exception for checked XML related exceptions.
- * 
+ *
  * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
  */
 public final class XMLException extends RuntimeException {
