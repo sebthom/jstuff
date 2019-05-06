0034cd4c1899d24b927445273791021fc9b04788
sebthom <sebthom@users.noreply.github.com>
2019-05-06 13:36:18 +0200

    trigger release build

--- .travis/release-trigger.sh
+++ .travis/release-trigger.sh
@@ -1,7 +1,7 @@
 # this file is evaluated by .travis/build.sh
 
-POM_CURRENT_VERSION="4.2.1-SNAPSHOT" # perform release if pom.xml matches this version
-POM_RELEASE_VERSION="4.2.1" # next release version
+POM_CURRENT_VERSION="5.0.0-SNAPSHOT" # perform release if pom.xml matches this version
+POM_RELEASE_VERSION="5.0.0" # next release version
 
 DRY_RUN=false # is dry run?
 SKIP_TESTS=true # skip tests?
