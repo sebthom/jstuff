1c38c8bf40ce8ef4e458406e491580cd9702346d
extern-thomschke-sebastian <extern.thomschke_sebastian@allianz.de>
2019-05-10 20:46:23 +0200

    add collection classes

---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/collection/EmptyList.java
@@ -0,0 +1,43 @@
+/*********************************************************************
+ * Copyright 2010-2019 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core.collection;
+
+import java.util.AbstractList;
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class EmptyList<E> extends AbstractList<E> {
+
+   @Override
+   public void add(final int index, final E element) {
+      // do nothing
+   }
+
+   @Override
+   public E get(final int index) {
+      throw new IndexOutOfBoundsException();
+   }
+
+   @Override
+   public E set(final int index, final E element) {
+      throw new IndexOutOfBoundsException();
+   }
+
+   @Override
+   public E remove(final int index) {
+      throw new IndexOutOfBoundsException();
+   }
+
+   @Override
+   public int size() {
+      return 0;
+   }
+}
---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/collection/EmptySet.java
@@ -0,0 +1,36 @@
+/*********************************************************************
+ * Copyright 2010-2019 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core.collection;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+
+import net.sf.jstuff.core.collection.iterator.Iterators;
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class EmptySet<E> extends AbstractSet<E> {
+
+   @Override
+   public boolean add(final E e) {
+      return true;
+   }
+
+   @Override
+   public Iterator<E> iterator() {
+      return Iterators.empty();
+   }
+
+   @Override
+   public int size() {
+      return 0;
+   }
+}
