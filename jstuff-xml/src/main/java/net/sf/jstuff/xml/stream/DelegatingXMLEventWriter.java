0ece8d768871a6731caed9a9491ca91e97f1c103
extern-thomschke-sebastian <extern.thomschke_sebastian@allianz.de>
2019-05-10 16:18:05 +0200

    Update doc

--- jstuff-xml/src/main/java/net/sf/jstuff/xml/stream/DelegatingXMLEventWriter.java
+++ jstuff-xml/src/main/java/net/sf/jstuff/xml/stream/DelegatingXMLEventWriter.java
@@ -1,3 +1,12 @@
+/*********************************************************************
+ * Copyright 2010-2019 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
 package net.sf.jstuff.xml.stream;
 
 import javax.xml.namespace.NamespaceContext;
@@ -8,6 +17,9 @@ import javax.xml.stream.events.XMLEvent;
 
 import net.sf.jstuff.core.types.Decorator;
 
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
 public class DelegatingXMLEventWriter extends Decorator.Default<XMLEventWriter> implements AutoCloseableXMLEventWriter {
 
    public DelegatingXMLEventWriter() {
