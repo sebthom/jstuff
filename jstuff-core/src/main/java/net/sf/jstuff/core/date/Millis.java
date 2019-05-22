dbf99d590bf7b9908730de5f983700520fb70402
sebthom <sebthom@users.noreply.github.com>
2019-05-22 15:42:36 +0200

    add Millis

---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/date/Millis.java
@@ -0,0 +1,63 @@
+/*********************************************************************
+ * Copyright 2010-2019 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core.date;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public abstract class Millis {
+
+   private static final long DAY_IN_MILLIS = TimeUnit.DAYS.toMillis(1);
+   private static final long HOUR_IN_MILLIS = TimeUnit.HOURS.toMillis(1);
+   private static final long MINUTE_IN_MILLIS = TimeUnit.MINUTES.toMillis(1);
+   private static final long SECOND_IN_MILLIS = TimeUnit.SECONDS.toMillis(1);
+
+   public static long fromDays(final int days) {
+      return days * DAY_IN_MILLIS;
+   }
+
+   public static long fromDays(final int days, final int hours) {
+      return days * DAY_IN_MILLIS + hours * HOUR_IN_MILLIS;
+   }
+
+   public static long fromDays(final int days, final int hours, final int minutes) {
+      return days * DAY_IN_MILLIS + hours * HOUR_IN_MILLIS + minutes * MINUTE_IN_MILLIS;
+   }
+
+   public static long fromDays(final int days, final int hours, final int minutes, final int seconds) {
+      return days * DAY_IN_MILLIS + hours * HOUR_IN_MILLIS + minutes * MINUTE_IN_MILLIS + seconds * SECOND_IN_MILLIS;
+   }
+
+   public static long fromHours(final int hours) {
+      return hours * HOUR_IN_MILLIS;
+   }
+
+   public static long fromHours(final int hours, final int minutes) {
+      return hours * HOUR_IN_MILLIS + minutes * MINUTE_IN_MILLIS;
+   }
+
+   public static long fromHours(final int hours, final int minutes, final int seconds) {
+      return hours * HOUR_IN_MILLIS + minutes * MINUTE_IN_MILLIS + seconds * SECOND_IN_MILLIS;
+   }
+
+   public static long fromMinutes(final int minutes) {
+      return minutes * MINUTE_IN_MILLIS;
+   }
+
+   public static long fromMinutes(final int minutes, final int seconds) {
+      return minutes * MINUTE_IN_MILLIS + seconds * SECOND_IN_MILLIS;
+   }
+
+   public static long fromSeconds(final int seconds) {
+      return seconds * SECOND_IN_MILLIS;
+   }
+}
---
+++ jstuff-core/src/test/java/net/sf/jstuff/core/date/MillisTest.java
@@ -0,0 +1,25 @@
+/*********************************************************************
+ * Copyright 2010-2019 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core.date;
+
+import junit.framework.TestCase;
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class MillisTest extends TestCase {
+
+   public void testMillis() {
+      assertEquals(Millis.fromDays(1), Millis.fromHours(24));
+      assertEquals(Millis.fromHours(1), Millis.fromMinutes(60));
+      assertEquals(Millis.fromMinutes(1), Millis.fromSeconds(60));
+      assertEquals(Millis.fromSeconds(1), 1_000);
+   }
+}
