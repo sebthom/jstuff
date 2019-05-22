75a201b150c44e16d5eac9a20bdb9d1297271760
sebthom <sebthom@users.noreply.github.com>
2019-05-22 15:44:47 +0200

    add getClientCertificate

--- jstuff-integration/src/main/java/net/sf/jstuff/integration/servlet/ServletUtils.java
+++ jstuff-integration/src/main/java/net/sf/jstuff/integration/servlet/ServletUtils.java
@@ -9,15 +9,35 @@
  *********************************************************************/
 package net.sf.jstuff.integration.servlet;
 
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jstuff.core.validation.Args;
 
 /**
  * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
  */
 public abstract class ServletUtils {
 
+   @SuppressWarnings("unchecked")
+   public static <T> T getAttribute(final HttpServletRequest request, final String name) {
+      return (T) request.getAttribute(name);
+   }
+
+   public static X509Certificate getClientCertificate(final HttpServletRequest request) {
+      Args.notNull("request", request);
+
+      final X509Certificate[] certChain = getAttribute(request, "javax.servlet.request.X509Certificate");
+      if (certChain == null || certChain.length == 0)
+         return null;
+      return certChain[0];
+   }
+
    public static String getContextURL(final HttpServletRequest request) {
+      Args.notNull("request", request);
 
       final String scheme = request.getScheme();
       final int port = request.getServerPort();
@@ -37,15 +57,35 @@ public abstract class ServletUtils {
       return url.toString();
    }
 
-   public static void preventCaching(final HttpServletResponse res) {
+   public static void requestBasicAuth(final HttpServletResponse response, final String realm) throws IOException {
+      response.setHeader("WWW-Authenticate", "Basic realm=\"" + realm + "\"");
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+   }
+
+   public static void setNoCachingHeaders(final HttpServletResponse response) {
+      Args.notNull("response", response);
+
       //http://www.onjava.com/pub/a/onjava/excerpt/jebp_3/index2.html
       // Set to expire far in the past.
-      res.setHeader("Expires", "Sat, 6 May 1995 12:00:00 GMT");
+      response.setHeader("Expires", "Sat, 6 May 1995 12:00:00 GMT");
       // Set standard HTTP/1.1 no-cache headers.
-      res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+      response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
       // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
-      res.addHeader("Cache-Control", "post-check=0, pre-check=0");
+      response.addHeader("Cache-Control", "post-check=0, pre-check=0");
       // Set standard HTTP/1.0 no-cache header.
-      res.setHeader("Pragma", "no-cache");
+      response.setHeader("Pragma", "no-cache");
+   }
+
+   public static void setRecommendedSecurityHeaders(final HttpServletResponse response, final boolean alwaysHTTPS) {
+      Args.notNull("response", response);
+
+      // https://www.globaldots.com/8-http-security-headers-best-practices/
+      response.setHeader("X-XSS-Protection", "1");
+      response.setHeader("X-Content-Type-Options", "nosniff");
+      response.setHeader("X-Frame-Options", "SAMEORIGIN");
+      if (alwaysHTTPS) {
+         response.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains");
+      }
+      response.setHeader("Referrer-Policy", "no-referrer");
    }
 }
