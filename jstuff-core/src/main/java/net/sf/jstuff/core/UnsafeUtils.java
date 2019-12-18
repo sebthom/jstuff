95bdd17a920b334f1f5f1ac63b1021c26f1d0791
sebthom <sebthom@users.noreply.github.com>
2019-12-18 13:31:42 +0100

    add UnsafeUtils

---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/UnsafeUtils.java
@@ -0,0 +1,35 @@
+/*********************************************************************
+ * Copyright 2010-2019 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core;
+
+import net.sf.jstuff.core.reflection.Fields;
+import net.sf.jstuff.core.validation.Args;
+import sun.misc.Unsafe; // CHECKSTYLE:IGNORE IllegalImport
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public abstract class UnsafeUtils {
+
+   private static final Unsafe UNSAFE = Fields.read(null, Fields.find(Unsafe.class, "theUnsafe"));
+
+   public static long addressOf(final Object obj) {
+      Args.notNull("obj", "obj");
+
+      switch (UNSAFE.addressSize()) {
+         case 4:
+            return UNSAFE.getInt(new Object[] {obj}, (long) UNSAFE.arrayBaseOffset(Object[].class));
+         case 8:
+            return UNSAFE.getLong(new Object[] {obj}, (long) UNSAFE.arrayBaseOffset(Object[].class));
+         default:
+            throw new IllegalStateException("Unsupported address size: " + UNSAFE.addressSize());
+      }
+   }
+}
---
+++ jstuff-core/src/test/java/net/sf/jstuff/core/UnsafeUtilsTest.java
@@ -0,0 +1,32 @@
+/*********************************************************************
+ * Copyright 2010-2019 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core;
+
+import static org.junit.Assert.*;
+
+import junit.framework.TestCase;
+
+/**
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class UnsafeUtilsTest extends TestCase {
+
+   public void testAddressOf() {
+      final Object obj1 = new Object();
+      assertTrue(UnsafeUtils.addressOf(obj1) > 0);
+
+      assertEquals(UnsafeUtils.addressOf(obj1), UnsafeUtils.addressOf(obj1));
+
+      final Object obj2 = new Object();
+      assertTrue(UnsafeUtils.addressOf(obj2) > 0);
+
+      assertNotEquals(UnsafeUtils.addressOf(obj1), UnsafeUtils.addressOf(obj2));
+   }
+}
