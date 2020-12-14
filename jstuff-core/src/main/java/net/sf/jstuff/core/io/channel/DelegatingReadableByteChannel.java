8132e4684921d7c53310c0f8340d09305112f17d
sebthom <sebthom@users.noreply.github.com>
2020-12-14 20:47:26 +0100

    add delegating byte channels

---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/io/channel/DelegatingReadableByteChannel.java
@@ -0,0 +1,73 @@
+/*********************************************************************
+ * Copyright 2010-2020 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core.io.channel;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+
+import net.sf.jstuff.core.functional.LongBiConsumer;
+import net.sf.jstuff.core.validation.Args;
+
+/**
+ * Delegating ReadableByteChannel with read callback.
+ *
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class DelegatingReadableByteChannel implements ReadableByteChannel {
+
+   private final ReadableByteChannel delegate;
+   private final LongBiConsumer onBytesRead;
+   private long totalBytesRead;
+
+   public DelegatingReadableByteChannel(final ReadableByteChannel delegate) {
+      this(delegate, null);
+   }
+
+   @SuppressWarnings("resource")
+   public DelegatingReadableByteChannel(final ReadableByteChannel delegate, final LongBiConsumer onBytesRead) {
+      Args.notNull("delegate", delegate);
+
+      this.delegate = delegate;
+      this.onBytesRead = onBytesRead == null ? (a, b) -> { /* ignore */ } : onBytesRead;
+   }
+
+   @Override
+   public void close() throws IOException {
+      delegate.close();
+   }
+
+   public ReadableByteChannel getDelegate() {
+      return delegate;
+   }
+
+   public long getTotalBytesRead() {
+      return totalBytesRead;
+   }
+
+   @Override
+   public boolean isOpen() {
+      return delegate.isOpen();
+   }
+
+   protected void onBytesRead(final int bytesRead) {
+      totalBytesRead += bytesRead;
+      onBytesRead.accept(bytesRead, totalBytesRead);
+   }
+
+   @Override
+   public int read(final ByteBuffer bb) throws IOException {
+      final int bytesRead = delegate.read(bb);
+      if (bytesRead > 0) {
+         onBytesRead(bytesRead);
+      }
+      return bytesRead;
+   }
+}
---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/io/channel/DelegatingWritableByteChannel.java
@@ -0,0 +1,73 @@
+/*********************************************************************
+ * Copyright 2010-2020 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.core.io.channel;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.WritableByteChannel;
+
+import net.sf.jstuff.core.functional.LongBiConsumer;
+import net.sf.jstuff.core.validation.Args;
+
+/**
+ * Delegating WritableByteChannel with write callback.
+ *
+ * @author <a href="http://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class DelegatingWritableByteChannel implements WritableByteChannel {
+
+   private final WritableByteChannel delegate;
+   private final LongBiConsumer onBytesWritten;
+   private long totalBytesWritten;
+
+   public DelegatingWritableByteChannel(final WritableByteChannel delegate) {
+      this(delegate, null);
+   }
+
+   @SuppressWarnings("resource")
+   public DelegatingWritableByteChannel(final WritableByteChannel delegate, final LongBiConsumer onBytesWritten) {
+      Args.notNull("delegate", delegate);
+
+      this.delegate = delegate;
+      this.onBytesWritten = onBytesWritten == null ? (a, b) -> { /* ignore */ } : onBytesWritten;
+   }
+
+   @Override
+   public void close() throws IOException {
+      delegate.close();
+   }
+
+   public WritableByteChannel getDelegate() {
+      return delegate;
+   }
+
+   public long getTotalBytesWritten() {
+      return totalBytesWritten;
+   }
+
+   @Override
+   public boolean isOpen() {
+      return delegate.isOpen();
+   }
+
+   protected void onBytesWritten(final int bytesWritten) {
+      totalBytesWritten += bytesWritten;
+      onBytesWritten.accept(bytesWritten, totalBytesWritten);
+   }
+
+   @Override
+   public int write(final ByteBuffer bb) throws IOException {
+      final int bytesWritten = delegate.write(bb);
+      if (bytesWritten > 0) {
+         onBytesWritten(bytesWritten);
+      }
+      return bytesWritten;
+   }
+}
