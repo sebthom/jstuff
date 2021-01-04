ea6274aec3a7e433269865aa27c657e9bddac35b
sebthom <sebthom@users.noreply.github.com>
2021-01-04 16:03:08 +0100

    add JsonNode/YamlNode

---
+++ jstuff-integration/src/main/java/net/sf/jstuff/integration/json/JsonNode.java
@@ -0,0 +1,200 @@
+/*********************************************************************
+ * Copyright 2010-2021 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.integration.json;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.RawValue;
+
+/**
+ * {@link ObjectNode} based JsonNode with improved fluent API.
+ *
+ * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
+ */
+public final class JsonNode extends ObjectNode {
+
+   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper(new JsonFactory());
+
+   private static final long serialVersionUID = 1L;
+
+   public static JsonNode create() {
+      return new JsonNode(OBJECT_MAPPER.getNodeFactory());
+   }
+
+   private JsonNode(final JsonNodeFactory nc) {
+      super(nc);
+   }
+
+   public JsonNode compute(final Consumer<JsonNode> consumer) {
+      consumer.accept(this);
+      return this;
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final BigDecimal v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final BigInteger v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final boolean v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final Boolean v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final byte[] v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final double v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final Double v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final float v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final Float v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final int v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final Integer v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final long v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final Long v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   public JsonNode put(final String fieldName, final Object obj) {
+      super.set(fieldName, obj == null ? null : OBJECT_MAPPER.valueToTree(obj));
+      return this;
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final short v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final Short v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode put(final String fieldName, final String v) {
+      return (JsonNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public JsonNode putNull(final String fieldName) {
+      return (JsonNode) super.putNull(fieldName);
+   }
+
+   @Override
+   public JsonNode putPOJO(final String fieldName, final Object pojo) {
+      return (JsonNode) super.putPOJO(fieldName, pojo);
+   }
+
+   @Override
+   public JsonNode putRawValue(final String fieldName, final RawValue raw) {
+      return (JsonNode) super.putRawValue(fieldName, raw);
+   }
+
+   @Override
+   public JsonNode remove(final Collection<String> fieldNames) {
+      return (JsonNode) super.remove(fieldNames);
+   }
+
+   @Override
+   public JsonNode remove(final String fieldName) {
+      return (JsonNode) super.remove(fieldName);
+   }
+
+   @Override
+   public JsonNode removeAll() {
+      return (JsonNode) super.removeAll();
+   }
+
+   @Override
+   public JsonNode retain(final Collection<String> fieldNames) {
+      return (JsonNode) super.retain(fieldNames);
+   }
+
+   @Override
+   public JsonNode retain(final String... fieldNames) {
+      return (JsonNode) super.retain(fieldNames);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public JsonNode set(final String fieldName, final com.fasterxml.jackson.databind.JsonNode value) {
+      return (JsonNode) super.set(fieldName, value);
+   }
+
+   @Override
+   public <T extends com.fasterxml.jackson.databind.JsonNode> T setAll(final Map<String, ? extends com.fasterxml.jackson.databind.JsonNode> properties) {
+      return super.setAll(properties);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public JsonNode setAll(final ObjectNode other) {
+      return (JsonNode) super.setAll(other);
+   }
+
+   @Override
+   public <T extends com.fasterxml.jackson.databind.JsonNode> T without(final Collection<String> fieldNames) {
+      return super.without(fieldNames);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public JsonNode without(final String fieldName) {
+      return (JsonNode) super.without(fieldName);
+   }
+}
---
+++ jstuff-integration/src/main/java/net/sf/jstuff/integration/yaml/YamlNode.java
@@ -0,0 +1,222 @@
+/*********************************************************************
+ * Copyright 2010-2021 by Sebastian Thomschke and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *********************************************************************/
+package net.sf.jstuff.integration.yaml;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.RawValue;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+
+/**
+ * {@link ObjectNode} based YamlNode with improved fluent API.
+ *
+ * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
+ */
+public final class YamlNode extends ObjectNode {
+
+   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper(new YAMLFactory() //
+      .enable(YAMLGenerator.Feature.MINIMIZE_QUOTES) //
+      .enable(YAMLGenerator.Feature.INDENT_ARRAYS) //
+      .disable(YAMLGenerator.Feature.SPLIT_LINES) //
+      .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER) //
+   );
+
+   private static final long serialVersionUID = 1L;
+
+   public static YamlNode create() {
+      return new YamlNode(OBJECT_MAPPER.getNodeFactory());
+   }
+
+   private YamlNode(final JsonNodeFactory nc) {
+      super(nc);
+   }
+
+   public YamlNode compute(final Consumer<YamlNode> consumer) {
+      consumer.accept(this);
+      return this;
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final BigDecimal v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final BigInteger v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final boolean v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final Boolean v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final byte[] v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final double v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final Double v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final float v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final Float v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final int v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final Integer v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final long v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final Long v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   public YamlNode put(final String fieldName, final Object obj) {
+      super.set(fieldName, obj == null ? null : OBJECT_MAPPER.valueToTree(obj));
+      return this;
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final short v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final Short v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode put(final String fieldName, final String v) {
+      return (YamlNode) super.put(fieldName, v);
+   }
+
+   @Override
+   public YamlNode putNull(final String fieldName) {
+      return (YamlNode) super.putNull(fieldName);
+   }
+
+   @Override
+   public YamlNode putPOJO(final String fieldName, final Object pojo) {
+      return (YamlNode) super.putPOJO(fieldName, pojo);
+   }
+
+   @Override
+   public YamlNode putRawValue(final String fieldName, final RawValue raw) {
+      return (YamlNode) super.putRawValue(fieldName, raw);
+   }
+
+   @Override
+   public YamlNode remove(final Collection<String> fieldNames) {
+      return (YamlNode) super.remove(fieldNames);
+   }
+
+   @Override
+   public YamlNode remove(final String fieldName) {
+      return (YamlNode) super.remove(fieldName);
+   }
+
+   @Override
+   public YamlNode removeAll() {
+      return (YamlNode) super.removeAll();
+   }
+
+   @Override
+   public YamlNode retain(final Collection<String> fieldNames) {
+      return (YamlNode) super.retain(fieldNames);
+   }
+
+   @Override
+   public YamlNode retain(final String... fieldNames) {
+      return (YamlNode) super.retain(fieldNames);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public YamlNode set(final String fieldName, final JsonNode value) {
+      return (YamlNode) super.set(fieldName, value);
+   }
+
+   @Override
+   public <T extends JsonNode> T setAll(final Map<String, ? extends JsonNode> properties) {
+      return super.setAll(properties);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public YamlNode setAll(final ObjectNode other) {
+      return (YamlNode) super.setAll(other);
+   }
+
+   @Override
+   public String toPrettyString() {
+      try {
+         return OBJECT_MAPPER.writeValueAsString(this);
+      } catch (final JsonProcessingException ex) {
+         return ex.getMessage();
+      }
+   }
+
+   @Override
+   public String toString() {
+      return toPrettyString();
+   }
+
+   @Override
+   public <T extends JsonNode> T without(final Collection<String> fieldNames) {
+      return super.without(fieldNames);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public YamlNode without(final String fieldName) {
+      return (YamlNode) super.without(fieldName);
+   }
+}
