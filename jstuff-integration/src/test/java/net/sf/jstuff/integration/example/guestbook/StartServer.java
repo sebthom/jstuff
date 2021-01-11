53749b5ec2479ea6d6bf5111a39d5c550cf78d6f
sebthom <sebthom@users.noreply.github.com>
2021-01-11 16:31:00 +0100

    fix compiler warning

--- jstuff-integration/src/test/java/net/sf/jstuff/integration/example/guestbook/StartServer.java
+++ jstuff-integration/src/test/java/net/sf/jstuff/integration/example/guestbook/StartServer.java
@@ -23,30 +23,25 @@ public final class StartServer {
 
    public static void main(final String[] args) throws Exception {
       final Server server = new Server();
-      final ServerConnector connector = new ServerConnector(server);
 
-      // Set some timeout options to make debugging easier.
-      connector.setIdleTimeout(10_000);
-      connector.setSoLingerTime(-1);
-      connector.setPort(8080);
-      server.addConnector(connector);
+      try (ServerConnector connector = new ServerConnector(server)) {
+         connector.setIdleTimeout(10_000); // set timeout to make debugging easier
+         connector.setPort(8080);
+         server.addConnector(connector);
 
-      final WebAppContext bb = new WebAppContext();
-      bb.setServer(server);
-      bb.setContextPath("/");
-      bb.setWar("src/test/resources/net/sf/jstuff/integration/example/guestbook/webapp");
-      server.setHandler(bb);
+         final WebAppContext bb = new WebAppContext();
+         bb.setServer(server);
+         bb.setContextPath("/");
+         bb.setWar("src/test/resources/net/sf/jstuff/integration/example/guestbook/webapp");
+         server.setHandler(bb);
 
-      try {
          System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
          server.start();
          System.in.read();
          System.out.println(">>> STOPPING EMBEDDED JETTY SERVER");
+      } finally {
          server.stop();
          server.join();
-      } catch (final Exception e) {
-         e.printStackTrace();
-         System.exit(1);
       }
    }
 
