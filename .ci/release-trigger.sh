9735bff4d3749d87e6920f1c84b68d3e510f4ae0
sebthom <sebthom@users.noreply.github.com>
2021-07-10 13:56:32 +0200

    update build config

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -4,4 +4,4 @@ POM_CURRENT_VERSION="5.14.3-SNAPSHOT" # perform release if pom.xml matches this 
 POM_RELEASE_VERSION="5.14.3" # next release version
 
 DRY_RUN=false # is dry run?
-SKIP_TESTS=true # skip tests?
+SKIP_TESTS=true # skip tests during release build?
