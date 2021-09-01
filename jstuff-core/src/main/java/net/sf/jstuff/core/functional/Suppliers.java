7eda6ae2177ded3b832d8b83fc97c516c4520234
sebthom <sebthom@users.noreply.github.com>
2021-09-01 17:13:42 +0200

    add suppliers

---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/functional/Suppliers.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2010-2021 by Sebastian Thomschke and contributors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package net.sf.jstuff.core.functional;
+
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import java.util.function.Supplier;
+
+import net.sf.jstuff.core.collection.tuple.Tuple2;
+import net.sf.jstuff.core.ref.LazyInitializedRef;
+import net.sf.jstuff.core.validation.Args;
+
+/**
+ * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
+ */
+public abstract class Suppliers {
+
+   public static <T> Supplier<T> memoize(final Supplier<? extends T> provider) {
+      Args.notNull("provider", provider);
+
+      return new Supplier<T>() {
+         private final LazyInitializedRef<T> cached = new LazyInitializedRef<T>() {
+            @Override
+            protected T create() {
+               return provider.get();
+            }
+         };
+
+         @Override
+         public T get() {
+            return cached.get();
+         }
+      };
+   }
+
+   public static <T> Supplier<T> memoize(final Supplier<? extends T> provider, final int ttl) {
+      Args.notNull("provider", provider);
+
+      return new Supplier<T>() {
+         private Tuple2<Long, T> cached;
+
+         @Override
+         public synchronized T get() {
+            if (cached != null && System.currentTimeMillis() - cached.get1() <= ttl)
+               return cached.get2();
+
+            final T obj = provider.get();
+            cached = new Tuple2<>(System.currentTimeMillis(), obj);
+            return obj;
+         }
+      };
+   }
+
+   public static <T> Supplier<T> memoizeSoft(final Supplier<? extends T> provider) {
+      Args.notNull("provider", provider);
+
+      return new Supplier<T>() {
+         private SoftReference<T> cached;
+
+         @Override
+         public synchronized T get() {
+            if (cached != null) {
+               final T val = cached.get();
+               if (val != null)
+                  return val;
+            }
+
+            final T obj = provider.get();
+            cached = new SoftReference<>(obj);
+            return obj;
+         }
+      };
+   }
+
+   public static <T> Supplier<T> memoizeSoft(final Supplier<? extends T> provider, final int ttl) {
+      Args.notNull("provider", provider);
+
+      return new Supplier<T>() {
+         private SoftReference<Tuple2<Long, T>> cached;
+
+         @Override
+         public synchronized T get() {
+            if (cached != null) {
+               final Tuple2<Long, T> val = cached.get();
+               if (val != null && System.currentTimeMillis() - val.get1() <= ttl)
+                  return val.get2();
+            }
+
+            final T obj = provider.get();
+            cached = new SoftReference<>(new Tuple2<>(System.currentTimeMillis(), obj));
+            return obj;
+         }
+      };
+   }
+
+   public static <T> Supplier<T> memoizeWeak(final Supplier<? extends T> provider) {
+      Args.notNull("provider", provider);
+
+      return new Supplier<T>() {
+         private WeakReference<T> cached;
+
+         @Override
+         public synchronized T get() {
+            if (cached != null) {
+               final T val = cached.get();
+               if (val != null)
+                  return val;
+            }
+
+            final T obj = provider.get();
+            cached = new WeakReference<>(obj);
+            return obj;
+         }
+      };
+   }
+
+   public static <T> Supplier<T> memoizeWeak(final Supplier<? extends T> provider, final int ttl) {
+      Args.notNull("provider", provider);
+
+      return new Supplier<T>() {
+         private WeakReference<Tuple2<Long, T>> cached;
+
+         @Override
+         public synchronized T get() {
+            if (cached != null) {
+               final Tuple2<Long, T> val = cached.get();
+               if (val != null && System.currentTimeMillis() - val.get1() <= ttl)
+                  return val.get2();
+            }
+
+            final T obj = provider.get();
+            cached = new WeakReference<>(new Tuple2<>(System.currentTimeMillis(), obj));
+            return obj;
+         }
+      };
+   }
+
+   public static <T> Supplier<T> of(final T object) {
+      return () -> object;
+   }
+
+   public static <T> Supplier<T> synchronizedSupplier(final Supplier<T> delegate) {
+      Args.notNull("delegate", delegate);
+
+      return () -> {
+         synchronized (delegate) {
+            return delegate.get();
+         }
+      };
+   }
+
+   public static <T> Supplier<T> synchronizedSupplier(final Supplier<T> delegate, final Object lock) {
+      Args.notNull("delegate", delegate);
+      Args.notNull("lock", lock);
+
+      return () -> {
+         synchronized (lock) {
+            return delegate.get();
+         }
+      };
+   }
+}
---
+++ jstuff-core/src/test/java/net/sf/jstuff/core/functional/SuppliersTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2010-2021 by Sebastian Thomschke and contributors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package net.sf.jstuff.core.functional;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.function.Supplier;
+
+import org.junit.Test;
+
+import net.sf.jstuff.core.concurrent.Threads;
+
+/**
+ * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class SuppliersTest {
+
+   @Test
+   public void testMemoize() {
+      final Supplier<Object> supplier = Suppliers.memoize(Object::new);
+      assertThat(supplier.get()).isSameAs(supplier.get());
+   }
+
+   @Test
+   public void testMemoizeSoft() {
+      final Supplier<Object> supplier = Suppliers.memoizeSoft(Object::new);
+      assertThat(supplier.get()).isSameAs(supplier.get());
+      final int hashCode = supplier.get().hashCode();
+      System.gc();
+      Threads.sleep(200);
+      System.gc();
+      Threads.sleep(200);
+      assertThat(supplier.get().hashCode()).isEqualTo(hashCode);
+   }
+
+   @Test
+   public void testMemoizeSoftWithTTL() {
+      final Supplier<Object> supplier = Suppliers.memoizeSoft(Object::new, 500);
+      assertThat(supplier.get()).isSameAs(supplier.get());
+      final Object obj = supplier.get();
+      Threads.sleep(501);
+      assertThat(obj).isNotSameAs(supplier.get());
+   }
+
+   @Test
+   public void testMemoizeWeak() {
+      final Supplier<Object> supplier = Suppliers.memoizeWeak(Object::new);
+      assertThat(supplier.get()).isSameAs(supplier.get());
+      final int hashCode = supplier.get().hashCode();
+      System.gc();
+      Threads.sleep(200);
+      System.gc();
+      Threads.sleep(200);
+      assertThat(supplier.get().hashCode()).isNotEqualTo(hashCode);
+   }
+
+   @Test
+   public void testMemoizeWeakWithTTL() {
+      final Supplier<Object> supplier = Suppliers.memoizeWeak(Object::new, 500);
+      assertThat(supplier.get()).isSameAs(supplier.get());
+      final Object obj = supplier.get();
+      Threads.sleep(501);
+      assertThat(obj).isNotSameAs(supplier.get());
+   }
+
+   @Test
+   public void testMemoizeWithTTL() {
+      final Supplier<Object> supplier = Suppliers.memoize(Object::new, 500);
+      assertThat(supplier.get()).isSameAs(supplier.get());
+      final Object obj = supplier.get();
+      Threads.sleep(501);
+      assertThat(obj).isNotSameAs(supplier.get());
+   }
+
+   @Test
+   public void testOf() {
+      final Object obj = new Object();
+      assertThat(Suppliers.of(obj).get()).isSameAs(obj);
+   }
+}
