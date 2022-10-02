98e1c44b659f5a08c585c62b08d971ca7c895003
sebthom <sebthom@users.noreply.github.com>
2022-10-02 21:17:21 +0200

    add test cases

---
+++ jstuff-core/src/test/java/net/sf/jstuff/core/reflection/SerializableFieldTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 by Sebastian Thomschke and contributors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package net.sf.jstuff.core.reflection;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+import net.sf.jstuff.core.io.SerializationUtils;
+
+/**
+ * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class SerializableFieldTest {
+
+   public String testField = "";
+
+   @Test
+   public void testSerializableField() throws SecurityException, NoSuchFieldException {
+      final var f = SerializableFieldTest.class.getDeclaredField("testField");
+
+      final var sf = new SerializableField(f);
+      assertThat(sf.getField()).isEqualTo(f);
+      assertThat(sf.getName()).isEqualTo("testField");
+      assertThat(sf.getDeclaringClass()).isEqualTo(SerializableFieldTest.class);
+
+      final SerializableField sf2 = SerializationUtils.deserialize(SerializationUtils.serialize(sf));
+      assertThat(sf2.getField()).isEqualTo(f);
+      assertThat(sf2.getName()).isEqualTo("testField");
+      assertThat(sf2.getDeclaringClass()).isEqualTo(SerializableFieldTest.class);
+   }
+}
---
+++ jstuff-core/src/test/java/net/sf/jstuff/core/reflection/SerializableMethodTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2022 by Sebastian Thomschke and contributors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package net.sf.jstuff.core.reflection;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+import net.sf.jstuff.core.io.SerializationUtils;
+
+/**
+ * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
+ */
+public class SerializableMethodTest {
+
+   @Test
+   public void testSerializableMethod() throws NoSuchMethodException, SecurityException {
+      final var m = SerializableMethodTest.class.getDeclaredMethod("testSerializableMethod");
+
+      final var sm = new SerializableMethod(m);
+      assertThat(sm.getMethod()).isEqualTo(m);
+      assertThat(sm.getName()).isEqualTo("testSerializableMethod");
+      assertThat(sm.getDeclaringClass()).isEqualTo(SerializableMethodTest.class);
+
+      final SerializableMethod sm2 = SerializationUtils.deserialize(SerializationUtils.serialize(sm));
+      assertThat(sm2.getMethod()).isEqualTo(m);
+      assertThat(sm2.getName()).isEqualTo("testSerializableMethod");
+      assertThat(sm2.getDeclaringClass()).isEqualTo(SerializableMethodTest.class);
+   }
+}
