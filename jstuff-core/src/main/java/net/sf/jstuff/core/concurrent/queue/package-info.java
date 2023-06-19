dca57d54a3b85c40857eb07ada14ed7b640aec0a
sebthom <sebthom@users.noreply.github.com>
2023-06-19 12:50:04 +0200

    add missing package-info

---
+++ jstuff-core/src/main/java/net/sf/jstuff/core/concurrent/queue/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: © Sebastian Thomschke and contributors
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * @author Sebastian Thomschke
+ */
+@NonNullByDefault
+package net.sf.jstuff.core.concurrent.queue;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
