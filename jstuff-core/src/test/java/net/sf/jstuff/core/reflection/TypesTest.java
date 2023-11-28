d727ea7fc83a10f74c30bbe497aab42a05a46a41
Sebastian Thomschke <sebthom@users.noreply.github.com>
2023-11-27 20:29:53 -0600

    Fix test case

--- jstuff-core/src/test/java/net/sf/jstuff/core/reflection/TypesTest.java
+++ jstuff-core/src/test/java/net/sf/jstuff/core/reflection/TypesTest.java
@@ -54,7 +54,7 @@ public class TypesTest {
    @Test
    public void testGetVersion() {
       // from META-INF/MANIFEST.MF
-      assertThat(Types.getVersion(ObjectUtils.class)).isEqualTo("3.13.0");
+      assertThat(Types.getVersion(ObjectUtils.class)).isEqualTo("3.14.0");
 
       // from META-INF/maven/.../pom.properties
       assertThat(Types.getVersion(JXPathContext.class)).isEqualTo("1.3");
