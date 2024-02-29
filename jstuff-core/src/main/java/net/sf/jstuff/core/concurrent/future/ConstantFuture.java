5358d23d3a251cae5b2b3466b492a20d90801a2c
sebthom <sebthom@users.noreply.github.com>
2024-02-29 13:15:05 +0100

    make ConstantFuture extend CompletableFuture

--- jstuff-core/src/main/java/net/sf/jstuff/core/concurrent/future/ConstantFuture.java
+++ jstuff-core/src/main/java/net/sf/jstuff/core/concurrent/future/ConstantFuture.java
@@ -4,46 +4,18 @@
  */
 package net.sf.jstuff.core.concurrent.future;
 
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author <a href="https://sebthom.de/">Sebastian Thomschke</a>
  */
-public class ConstantFuture<T> implements Future<T> {
+public class ConstantFuture<T> extends CompletableFuture<T> {
 
    public static <T> ConstantFuture<T> of(final T value) {
       return new ConstantFuture<>(value);
    }
 
-   private final T value;
-
    public ConstantFuture(final T value) {
-      this.value = value;
-   }
-
-   @Override
-   public boolean cancel(final boolean mayInterruptIfRunning) {
-      return false;
-   }
-
-   @Override
-   public boolean isCancelled() {
-      return false;
-   }
-
-   @Override
-   public boolean isDone() {
-      return true;
-   }
-
-   @Override
-   public T get() {
-      return value;
-   }
-
-   @Override
-   public T get(final long timeout, final TimeUnit unit) {
-      return get();
+      complete(value);
    }
 }
