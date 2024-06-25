9d75f0e66b1a7bcdbeec9b872ffa4685bb8e3910
sebthom <sebthom@users.noreply.github.com>
2024-06-25 21:03:10 +0200

    trigger release build

--- .ci/release-trigger.sh
+++ .ci/release-trigger.sh
@@ -1,7 +1,7 @@
 # this file is evaluated by .ci/build.sh
 
-POM_CURRENT_VERSION="7.10.1-SNAPSHOT" # perform release if pom.xml matches this version
-POM_RELEASE_VERSION="7.11.0" # next release version
+POM_CURRENT_VERSION="7.11.1-SNAPSHOT" # perform release if pom.xml matches this version
+POM_RELEASE_VERSION="7.12.0" # next release version
 
 DRY_RUN=false # is dry run?
 SKIP_TESTS=true # skip tests during release build?
